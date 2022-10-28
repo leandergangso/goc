bc73026d15b6c2318f7f3edbb6bc7d4bf9d178ca
Einar <mail@einargangso.no>
2022-10-28 09:11:32 +0200

    Cleanup the setup progress (#3)

--- file.go
+++ file.go
@@ -6,11 +6,16 @@ import (
 	"os"
 )
 
-var SHARED_PATH = os.Getenv("HOME") + "/.goc_cli/"
+var dirname, err = os.UserConfigDir()
+
+var SHARED_PATH = dirname + "/goc_cli/"
 var FILE_NAME = "data.json"
 
 func readFile() *FileData {
 	path := SHARED_PATH + FILE_NAME
+
+	createDataFileIfNotExists(path)
+
 	f, err := os.Open(path)
 	if err != nil {
 		log.Fatalf("Unable to open file: %v", err)
@@ -26,6 +31,9 @@ func readFile() *FileData {
 
 func writeToFile(data *FileData) {
 	path := SHARED_PATH + FILE_NAME
+
+	createDataFileIfNotExists(path)
+
 	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
 	if err != nil {
 		log.Fatalf("Unable to open file: %v", err)
@@ -36,3 +44,16 @@ func writeToFile(data *FileData) {
 		log.Fatalf("Unable to write to file: %v", err)
 	}
 }
+
+func createDataFileIfNotExists(path string) {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		data := []byte("{}")
+		err := os.WriteFile(path, data, 0644)
+
+		if (err != nil) {
+			log.Fatal(err)
+		}
+	}
+}
+
+
